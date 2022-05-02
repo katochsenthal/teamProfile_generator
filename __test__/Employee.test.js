7ce9bb45eb0c960c2b093b15de5fb4e68c3b7fb0
katochsenthal <katoch.senthal@gmail.com>
2022-05-02 19:35:00 -0400

    created a test for employee

---
+++ __test__/Employee.test.js
@@ -0,0 +1,8 @@
+const Employee = require("../lib/Employee");
+
+test("creating employee object", () => {
+  const employee = new Employee("Sam", "11", "sam@noemail.com");
+  expect(employee.name).toBe("Sam");
+  expect(employee.id).toBe("11");
+  expect(employee.email).toBe("sam@noemail.com");
+});
