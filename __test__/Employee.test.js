f2160006b43ec5d2ff9b93cd02deab14a2d3a527
katochsenthal <katoch.senthal@gmail.com>
2022-05-04 18:24:53 -0400

    completed employee class

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -2,6 +2,7 @@ const Employee = require("../lib/Employee");
 
 test("creating employee object", () => {
   const employee = new Employee("Sam", "11", "sam@noemail.com");
+
   expect(employee.name).toBe("Sam");
   expect(employee.id).toBe("11");
   expect(employee.email).toBe("sam@noemail.com");
---
+++ __test__/Engineer.test.js
@@ -0,0 +1,10 @@
+const Engineer = require("../lib/Engineer");
+
+test("creates an Engineer object", () => {
+  const engineer = new Engineer("Alec", "2", "alec@fakeemail.com", "dsada");
+
+  expect(engineer.name).toBe("Alec");
+  expect(engineer.id).toBe("2");
+  expect(engineer.email).toBe("alec@fakeemail.com");
+  expect(engineer.github).toBe("dsada");
+});
