7cda0f61ae382a146f440e6a9e7e93fa640c21d8
katochsenthal <katoch.senthal@gmail.com>
2022-05-07 14:05:09 -0400

    generate html successfully

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -4,6 +4,8 @@ const generateHtml = (data) => {
   const generateManager = (manager) => {
     const { name, id, email, officeNumber } = manager;
     let managerHtml = `
+
+    
         <div class="card bg-primary mb-3" style="width: 18rem">
         <div class="card-body">
           <h5 class="card-title">${name}</h5>
@@ -68,4 +70,36 @@ const generateHtml = (data) => {
   return html.join("");
 };
 
-module.exports = { generateHtml };
+module.exports = (data) => {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="../dist/style.css" />
+    <title>Team Profile Generator</title>
+  </head>
+  <body>
+    <nav class="navbar fixed-top navbar-danger bg-danger">
+      <a class="navbar-brand text-white h4 ml-auto">My Team</a>
+    </nav>
+
+    <main class="row justify-content-center">${generateHtml(data)}</main>
+  </body>
+</html>
+`;
+};
+
+// module.exports = { generateHtml };
